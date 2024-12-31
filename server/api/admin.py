b2c434bc4f750c0e0619724e2a5c2fd51fea8764
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2024-12-31 17:35:00 +0530

    register models with admin

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import UserProfile, UserHandler, IndividualStats
 
-# Register your models here.
+admin.site.register(UserProfile)
+admin.site.register(UserHandler)
+admin.site.register(IndividualStats)
