a2b2bc90199e8c83dbdd21f0efc12f2b91fa6310
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-01 01:08:14 +0530

    add views for user profile

--- server/api/views.py
+++ server/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import UserProfile, UserHandler, IndividualStats
+from .serializers import UserProfileSerializer, UserHandlerSerializer, IndividualStatsSerializer
+from rest_framework import generics
 
-# Create your views here.
+class UserProfileDetailAPIView(generics.RetrieveAPIView):
+    """ Get details of a single user """
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
+    lookup_field = 'pk'
+
+class UserProfileListCreateAPIView(generics.ListCreateAPIView):
+    """ Get list of all users or create a new user """
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
