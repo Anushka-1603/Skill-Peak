ebad2b13af756453992d17529de727dc25c7de9a
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-01 01:26:49 +0530

    add endpoint for viewing individual stats

--- server/api/views.py
+++ server/api/views.py
@@ -24,3 +24,9 @@ class UserHandlerListCreateAPIView(generics.ListCreateAPIView):
     """ Get list of all user handlers or create a new user handler """
     queryset = UserHandler.objects.all()
     serializer_class = UserHandlerSerializer
+    
+class IndividualStatsRetrieveAPIView(generics.RetrieveAPIView):
+    """ Get stats associated with a single user handler """
+    queryset = IndividualStats.objects.all()
+    serializer_class = IndividualStatsSerializer
+    lookup_field = 'pk'
