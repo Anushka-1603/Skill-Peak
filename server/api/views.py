583cb3a40c0161f4a8b4840767e41f7bb6543cc4
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-01 18:27:51 +0530

    modify user handler get and create view

--- server/api/views.py
+++ server/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import UserProfile, UserHandler, IndividualStats
 from .serializers import UserProfileSerializer, UserHandlerSerializer, IndividualStatsSerializer
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 class UserProfileDetailAPIView(generics.RetrieveAPIView):
     """ Get details of a single user """
@@ -22,8 +23,19 @@ class UserHandlerDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
 
 class UserHandlerListCreateAPIView(generics.ListCreateAPIView):
     """ Get list of all user handlers or create a new user handler """
-    queryset = UserHandler.objects.all()
+    # queryset = UserHandler.objects.all()
     serializer_class = UserHandlerSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def get_queryset(self):
+        # a logged in user can view only the user handlers he has added
+        user_profile = UserProfile.objects.get(email=self.request.user.email)
+        return UserHandler.objects.filter(user=user_profile)
+    
+    def perform_create(self, serializer):
+        # Automatically associate the handler with the logged-in user's profile
+        user_profile = UserProfile.objects.get(email=self.request.user.email)
+        serializer.save(user=user_profile)
     
 class IndividualStatsRetrieveAPIView(generics.RetrieveAPIView):
     """ Get stats associated with a single user handler """
