4d16d87060da36917070f98a849dbe4413943f41
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-03 20:12:11 +0530

    update admin.py

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import UserProfile, UserHandler, IndividualStats
 
-admin.site.register(UserProfile)
-admin.site.register(UserHandler)
-admin.site.register(IndividualStats)
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ['name', 'email', 'created_at']
+    
+class AdminUserHandler(admin.ModelAdmin):
+    list_display = ['handlername', 'handlerid', 'user', 'platform', 'created_at']
+    
+class AdminIndividualStats(admin.ModelAdmin):
+    list_display = ['handlerid', 'contributions', 'submissions', 'recorded_at']
+
+admin.site.register(UserProfile, AdminUserProfile)
+admin.site.register(UserHandler, AdminUserHandler)
+admin.site.register(IndividualStats, AdminIndividualStats)
