25f0178fbcc396f3f67eea4ae059a6ac39bc7cea
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-10 15:25:35 +0530

    add view to update stats and return top coders

--- server/api/views.py
+++ server/api/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import UserProfile, UserHandler, IndividualStats
 from .serializers import UserProfileSerializer, UserHandlerSerializer, IndividualStatsSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from .scrapers import scrape_leetcode, scrape_github
+import datetime
 
 class UserProfileDetailAPIView(generics.RetrieveAPIView):
     """ Get details of a single user """
@@ -46,3 +48,44 @@ class IndividualStatsRetrieveAPIView(generics.RetrieveAPIView):
     queryset = IndividualStats.objects.all()
     serializer_class = IndividualStatsSerializer
     lookup_field = 'pk'
+    
+class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
+    """ Update stats or create new stat associated with a single user handler """
+    queryset = IndividualStats.objects.all()
+    serializer_class = IndividualStatsSerializer
+    
+    def get(self, request, *args, **kwargs):
+        userHandlers = UserHandler.objects.all()
+        
+        for userHandler in userHandlers:
+            if userHandler.platform == userHandler.LEETCODE:
+                submissions = scrape_leetcode(userHandler.handlerid)
+                contributions = 0
+                
+            elif userHandler.platform == userHandler.GITHUB:
+                contributions = scrape_github(userHandler.handlerid)
+                submissions = 0
+            
+            # if individual stats already exist, create it, else create new
+            indiv_stat, created = IndividualStats.objects.update_or_create(
+                handlerid = userHandler, 
+                defaults = { 
+                    'contributions': contributions, 
+                    'submissions': submissions, 
+                    'recorded_at': datetime.datetime.now() 
+                }
+            )
+            
+        # finding top 3 coders of the week
+        leetcode_top_coders = self.get_queryset().filter(handlerid__platform=UserHandler.LEETCODE).order_by('-submissions')[:3]
+        github_top_coders = self.get_queryset().filter(handlerid__platform=UserHandler.GITHUB).order_by('-contributions')[:3]
+        
+        leetcode_serializer = self.get_serializer(leetcode_top_coders, many=True)
+        github_serializer = self.get_serializer(github_top_coders, many=True)
+        
+        return Response(
+            {
+                'leetcode': leetcode_serializer.data,
+                'github': github_serializer.data
+            }
+        )
