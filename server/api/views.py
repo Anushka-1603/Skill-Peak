358c54086db0f2b7beee27bf8226f4cd51250671
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-12 22:48:04 +0530

    Add two more api views

--- server/api/views.py
+++ server/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics
 from rest_framework.response import Response
 from .scrapers import scrape_leetcode, scrape_github
 import datetime
+import json
 
 class UserProfileDetailAPIView(generics.RetrieveAPIView):
     """ Get details of a single user """
@@ -38,16 +39,28 @@ class UserHandlerListCreateAPIView(generics.ListCreateAPIView):
     #     # print(res)
     #     return res
     
-    # def perform_create(self, serializer):
-    #     # Automatically associate the handler with the logged-in user's profile
-    #     user_profile = UserProfile.objects.get(email=self.request.user.email)
-    #     serializer.save(user=user_profile)
-
 class IndividualStatsRetrieveAPIView(generics.RetrieveAPIView):
-    """ Get stats associated with a single user handler """
+    """ Get stats associated with a single user handler for any given number of days"""
     queryset = IndividualStats.objects.all()
     serializer_class = IndividualStatsSerializer
-    lookup_field = 'pk'
+        
+    def get(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        handler_id = data.get('handler_id')
+        days = data.get('days')
+        platform = data.get('platform')        
+
+        if platform == 'leetcode':
+            submissions = scrape_leetcode(handler_id,days)
+            contributions = 0
+            return Response({'Leetcode submissions':submissions})
+        elif platform == 'github':
+            contributions = scrape_github(handler_id,days)
+            submissions = 0
+            return Response({'Github contributions':contributions})
+        else:
+            return Response({'message':'Invalid platform'})
+            
     
 class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
     """ Update stats or create new stat associated with a single user handler """
@@ -59,11 +72,11 @@ class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
         
         for userHandler in userHandlers:
             if userHandler.platform == userHandler.LEETCODE:
-                submissions = scrape_leetcode(userHandler.handlerid)
+                submissions = scrape_leetcode(userHandler.handlerid,7)
                 contributions = 0
                 
             elif userHandler.platform == userHandler.GITHUB:
-                contributions = scrape_github(userHandler.handlerid)
+                contributions = scrape_github(userHandler.handlerid,7)
                 submissions = 0
             
             # if individual stats already exist, create it, else create new
@@ -89,3 +102,13 @@ class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
                 'github': github_serializer.data
             }
         )
+        
+class UserHandlerofUserAPIView(generics.ListAPIView):
+    """ Get all Userhandlers of a particular User """
+    queryset = UserHandler.objects.all()
+    serializer_class = UserHandlerSerializer
+    lookup_field = 'user_id'
+    
+    def get_queryset(self):
+        user_profile = UserProfile.objects.get(id=self.kwargs['user_id'])
+        return UserHandler.objects.filter(user=user_profile)
