a3c611f988c09b5cc6db7cd7e7ddf302b09d87ac
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-24 02:32:08 +0530

    add token_blacklist for logout view

--- server/server/settings.py
+++ server/server/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'api',
     'django_celery_results',
     'django_celery_beat',
