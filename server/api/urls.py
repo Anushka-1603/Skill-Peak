7f1b2e19d90fe7e1428318555c1748b0b219ee3a
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-24 02:44:50 +0530

    update urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -4,22 +4,13 @@ from . import auth
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    # user profile endpoints
     path('', views.UserProfileListCreateAPIView.as_view()),
     path('<int:pk>/', views.UserProfileDetailAPIView.as_view()),
-    
-    # user handler endpoints
     path('userhandlers/', views.UserHandlerListCreateAPIView.as_view()),
-    path('userhandlers/<int:pk>/', views.UserHandlerDetailAPIView.as_view()),
-        
+    path('userhandler/', views.UserHandlerDetailAPIView.as_view()),
     path('userhandlers/viewhandler/', views.IndividualStatsRetrieveAPIView.as_view()),
-    
-    path('user/userhandlers/<int:user_id>', views.UserHandlerofUserAPIView.as_view()),
-    
-    # update stats endpoint
+    path('userhandlers/all/', views.UserHandlersAllAPIView.as_view()),    
     path('userhandlers/update-stats/', views.UpdateStatsAPIView.as_view()),
-    
-    # authentication endpoints
     path('auth/register/', auth.RegisterView.as_view(), name='register'),
     path('auth/login/', auth.LoginView.as_view(), name='login'),
     path('auth/logout/', auth.LogoutView.as_view(), name='logout'),
