e5d6415c5c8e4945c0ddf924de6f081bdc983ab6
Anushka-1603 <120577968+Anushka-1603@users.noreply.github.com>
2025-01-24 02:43:37 +0530

    add authentication details

--- server/api/views.py
+++ server/api/views.py
@@ -2,60 +2,119 @@ from django.shortcuts import render
 from .models import UserProfile, UserHandler, IndividualStats
 from .serializers import UserProfileSerializer, UserHandlerSerializer, IndividualStatsSerializer
 from rest_framework import generics
+from rest_framework import permissions, authentication
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound, PermissionDenied
 from .scrapers import scrape_leetcode, scrape_github
 import datetime
 import json
 
 class UserProfileDetailAPIView(generics.RetrieveAPIView):
     """ Get details of a single user """
+    
+    permission_classes = [permissions.IsAdminUser]
+    
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     lookup_field = 'pk'
 
 class UserProfileListCreateAPIView(generics.ListCreateAPIView):
     """ Get list of all users or create a new user """
+    
+    permission_classes = [permissions.IsAdminUser]
+    
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
     
 class UserHandlerDetailAPIView(generics.RetrieveAPIView, generics.DestroyAPIView):
     """ Get or delete details of a single user handler """
+    
+    permission_classes = [permissions.IsAuthenticated]
+    
     queryset = UserHandler.objects.all()
     serializer_class = UserHandlerSerializer
-    lookup_field = 'pk'
-
+    
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        
+        try:
+            user_handler = UserHandler.objects.get(handlerid=data['handlerid'], platform=data['platform'])
+        except UserHandler.DoesNotExist:
+            raise NotFound(f"{str(data['platform'])} handler with id {str(data['handlerid'])} does not exist.")
+        
+        if user_handler.user != user and not user.is_staff:
+            raise PermissionDenied(f"You do not have permission to access {str(user_handler.handlername)}.")
+        
+        serializer = self.get_serializer(user_handler)
+        return Response(serializer.data)
+    
+    def delete(self, request, *args, **kwargs):
+        user = request.user
+        data = request.data
+        
+        try:
+            user_handler = UserHandler.objects.get(handlerid=data['handlerid'], platform=data['platform'])
+        except UserHandler.DoesNotExist:
+            raise NotFound(f"{str(data['platform'])} handler with id {str(data['handlerid'])} does not exist.")
+        
+        if user_handler.user != user and not user.is_staff:
+            raise PermissionDenied(f"You do not have permission to delete {str(user_handler.handlername)}")
+        
+        user_handler.delete()
+        return Response({'message':'User Handler deleted successfully'})
+        
 class UserHandlerListCreateAPIView(generics.ListCreateAPIView):
     """ Get list of all user handlers or create a new user handler """
+    
+    permission_classes = [permissions.IsAuthenticated]
+    
     queryset = UserHandler.objects.all()
     serializer_class = UserHandlerSerializer
-    # permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # a logged in user can view only the user handlers he has added
+        user_profile = self.request.user
+        if user_profile.is_staff:
+            res = UserHandler.objects.all()
+        res = UserHandler.objects.filter(user=user_profile)
+        return res
     
-    # def get_queryset(self):
-    #     # a logged in user can view only the user handlers he has added
-    #     print(self.request.user.email)
-    #     user_profile = UserProfile.objects.get(email=self.request.user.email)
-    #     # print(user_profile)
-    #     res = UserHandler.objects.filter(user=user_profile)
-    #     # print(res)
-    #     return res
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        user = request.user
+        data['user'] = user.id
+        serializer = self.get_serializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
     
 class IndividualStatsRetrieveAPIView(generics.RetrieveAPIView):
     """ Get stats associated with a single user handler for any given number of days"""
     queryset = IndividualStats.objects.all()
     serializer_class = IndividualStatsSerializer
+    permission_classes = [permissions.IsAuthenticated]
         
     def get(self, request, *args, **kwargs):
-        data = json.loads(request.body)
-        handler_id = data.get('handler_id')
+        data = request.data
+        # data = json.loads(request.body)
+        handlerid = data.get('handlerid')
         days = data.get('days')
         platform = data.get('platform')        
-
+        
+        # Check if the UserHandler is associated with the logged-in user's UserProfile
+        try:
+            user_handler = UserHandler.objects.get(handlerid=handlerid, user=request.user)
+        except UserHandler.DoesNotExist:
+            raise PermissionDenied("You do not have permission to access this resource.")
+        
         if platform == 'leetcode':
-            submissions = scrape_leetcode(handler_id,days)
+            submissions = scrape_leetcode(handlerid,days)
             contributions = 0
             return Response({'Leetcode submissions':submissions})
         elif platform == 'github':
-            contributions = scrape_github(handler_id,days)
+            contributions = scrape_github(handlerid,days)
             submissions = 0
             return Response({'Github contributions':contributions})
         else:
@@ -66,6 +125,7 @@ class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
     """ Update stats or create new stat associated with a single user handler """
     queryset = IndividualStats.objects.all()
     serializer_class = IndividualStatsSerializer
+    permission_classes = [permissions.IsAdminUser]
     
     def get(self, request, *args, **kwargs):
         userHandlers = UserHandler.objects.all()
@@ -103,12 +163,10 @@ class UpdateStatsAPIView(generics.UpdateAPIView, generics.CreateAPIView):
             }
         )
         
-class UserHandlerofUserAPIView(generics.ListAPIView):
-    """ Get all Userhandlers of a particular User """
+class UserHandlersAllAPIView(generics.ListAPIView):
+    """ Get all Userhandlers """
+    
+    permission_classes = [permissions.IsAdminUser]
+    
     queryset = UserHandler.objects.all()
     serializer_class = UserHandlerSerializer
-    lookup_field = 'user_id'
-    
-    def get_queryset(self):
-        user_profile = UserProfile.objects.get(id=self.kwargs['user_id'])
-        return UserHandler.objects.filter(user=user_profile)
